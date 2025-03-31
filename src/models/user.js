588ada7d686b77000c06e55fef18648ce974268e
Code Fusion <codefusion.tech.in@gmail.com>
2025-03-31 21:14:41 +0530

    Update user.js

--- src/models/user.js
+++ src/models/user.js
@@ -6,30 +6,6 @@ const UserSchema = new mongoose.Schema(
     otp: { type: String },
     otpExpiresAt: { type: Date },
     //chnages
-      name: {
-    type: String,
-    trim: true,
-    maxlength: [50, 'Name cannot be more than 50 characters']
-  },
-  phone: {
-    type: String,
-    trim: true,
-    match: [/^[0-9]{10,15}$/, 'Please fill a valid phone number']
-  },
-  image: {
-    type: String,
-    default: null
-  },
-  location: {
-    city: {
-      type: String,
-      trim: true
-    },
-    coordinates: {
-      type: [Number], // [longitude, latitude]
-      default: null,
-      index: '2dsphere'
-    }
   },
   { timestamps: true }
 );
