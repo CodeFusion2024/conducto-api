5a0649f881f73297e6e6820869720c1db6af0a4a
CodeFusion2024 <codefusion.tech.in@gmail.com>
2025-04-04 06:05:21 +0530

    uid cat

--- src/models/Category.js
+++ src/models/Category.js
@@ -1,15 +1,17 @@
 import mongoose from "mongoose";
+import { v4 as uuidv4 } from "uuid"; // Import UUID
 
 const categorySchema = new mongoose.Schema(
   {
+    categoryId: { type: String, unique: true, default: uuidv4 }, // Unique Category ID
     name: { type: String, required: true, trim: true },
     description: { type: String },
     store: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Store",
       required: true,
-    }, // Linked to Store
-    products: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }], // Products under this category
+    },
+    products: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }],
   },
   { timestamps: true }
 );
